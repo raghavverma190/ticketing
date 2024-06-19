5925833cf43088ae27c751ee372ac28fa26340b0
Raghav Verma <raghav.verma190@gmail.com>
2024-06-20 04:24:18 +0530

    Added password hashing

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import { Password } from '../services/password';
 
 // An interface that describes the propereties that are required to create a new user
 interface UserAttrs {
@@ -28,6 +29,15 @@ const userSchema = new mongoose.Schema({
     required: true,
   },
 });
+
+userSchema.pre('save', async function (done) {
+  if (this.isModified('password')) {
+    const hashed = await Password.toHash(this.get('password'));
+    this.set('password', hashed);
+  }
+  done();
+});
+
 userSchema.statics.build = (attrs: UserAttrs) => {
   return new User(attrs);
 };
