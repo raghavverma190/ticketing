fb3f3a636fcfed813870f2ecec1b49aa642fc31c
Raghav Verma <raghav.verma190@gmail.com>
2024-07-11 18:42:20 +0530

    Using environment variable for MONGO_URI

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -6,8 +6,11 @@ const start = async () => {
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined!');
   }
+  if (!process.env.MONGO_URI) {
+    throw new Error('JWT_KEY must be defined!');
+  }
   try {
-    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
+    await mongoose.connect(process.env.MONGO_URI);
     console.log('Connected to MongoDB of auth service');
   } catch (err) {
     console.log(err);
