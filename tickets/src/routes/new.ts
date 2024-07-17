b989e1ab40c20d76553a9d55c5d528d71796fb3a
Raghav Verma <raghav.verma190@gmail.com>
2024-07-18 03:16:54 +0530

    Adding Auth protection and faking authentication during tests for ticket service

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -1,8 +1,9 @@
 import express, { Request, Response } from 'express';
+import { requireAuth } from '@rvtickets190/common';
 
 const router = express.Router();
 
-router.post('/api/tickets', (req: Request, res: Response) => {
+router.post('/api/tickets', requireAuth, (req: Request, res: Response) => {
   res.sendStatus(200);
 });
 
